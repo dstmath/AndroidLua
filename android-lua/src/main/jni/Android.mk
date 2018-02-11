9691ffecb934752ad32ddf78c2246ad2a0f9afc8
dstmath <dstmath@163.com>
2018-02-11 09:29:21 +0800

    optimize Android.mk

--- android-lua/src/main/jni/Android.mk
+++ android-lua/src/main/jni/Android.mk
@@ -1,10 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-SRC :=./lua-5.3.4/src
+LUA_SRC :=$(LOCAL_PATH)/lua-5.3.4/src
 LOCAL_MODULE    := lua
-LOCAL_SRC_FILES := lua-5.3.4/src/lapi.c lua-5.3.4/src/lauxlib.c lua-5.3.4/src/lbaselib.c lua-5.3.4/src/lbitlib.c lua-5.3.4/src/lcode.c lua-5.3.4/src/lcorolib.c lua-5.3.4/src/lctype.c lua-5.3.4/src/ldblib.c lua-5.3.4/src/ldebug.c lua-5.3.4/src/ldo.c lua-5.3.4/src/ldump.c lua-5.3.4/src/lfunc.c lua-5.3.4/src/lgc.c lua-5.3.4/src/linit.c lua-5.3.4/src/liolib.c lua-5.3.4/src/llex.c lua-5.3.4/src/lmathlib.c lua-5.3.4/src/lmem.c lua-5.3.4/src/loadlib.c lua-5.3.4/src/lobject.c lua-5.3.4/src/lopcodes.c lua-5.3.4/src/loslib.c lua-5.3.4/src/lparser.c lua-5.3.4/src/lstate.c lua-5.3.4/src/lstring.c lua-5.3.4/src/lstrlib.c lua-5.3.4/src/ltable.c lua-5.3.4/src/ltablib.c lua-5.3.4/src/ltm.c lua-5.3.4/src/lundump.c lua-5.3.4/src/lutf8lib.c lua-5.3.4/src/lvm.c lua-5.3.4/src/lzio.c
-
+LOCAL_SRC_FILES := $(LUA_SRC)/lapi.c $(LUA_SRC)/lauxlib.c $(LUA_SRC)/lbaselib.c $(LUA_SRC)/lbitlib.c $(LUA_SRC)/lcode.c $(LUA_SRC)/lcorolib.c $(LUA_SRC)/lctype.c $(LUA_SRC)/ldblib.c $(LUA_SRC)/ldebug.c $(LUA_SRC)/ldo.c $(LUA_SRC)/ldump.c $(LUA_SRC)/lfunc.c $(LUA_SRC)/lgc.c $(LUA_SRC)/linit.c $(LUA_SRC)/liolib.c $(LUA_SRC)/llex.c $(LUA_SRC)/lmathlib.c $(LUA_SRC)/lmem.c $(LUA_SRC)/loadlib.c $(LUA_SRC)/lobject.c $(LUA_SRC)/lopcodes.c $(LUA_SRC)/loslib.c $(LUA_SRC)/lparser.c $(LUA_SRC)/lstate.c $(LUA_SRC)/lstring.c $(LUA_SRC)/lstrlib.c $(LUA_SRC)/ltable.c $(LUA_SRC)/ltablib.c $(LUA_SRC)/ltm.c $(LUA_SRC)/lundump.c $(LUA_SRC)/lutf8lib.c $(LUA_SRC)/lvm.c $(LUA_SRC)/lzio.c
+#LOCAL_SRC_FILES := $(wildcard $(LUA_SRC)/*.c)
 LOCAL_CFLAGS += -DLUA_DL_DLOPEN -DLUA_USE_C89 -DLUA_COMPAT_5_1 -DLUA_COMPAT_5_2 -DLUA_USE_LINUX
 
 LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog -ldl
