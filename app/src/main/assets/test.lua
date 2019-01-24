af02e3c26e8eeb541be525847f986378dc5b3e8f
toor <dstmath@163.com>
2019-01-24 11:15:25 +0800

    clean code

--- app/src/main/assets/test.lua
+++ app/src/main/assets/test.lua
@@ -3,6 +3,8 @@ toast:show()
 
 -- 注释 startActivity
 local intent = luajava.newInstance("android.content.Intent")
+-- targetSdk = 22时必须加此NEW_TASK
+intent:addFlags(0x10000000)
 intent:setClassName("com.android.settings", "com.android.settings.Settings")
 
 local context = context()
