6ece86fef8127a60a5dc484cab12bb85a2ce8596
toor <dstmath@163.com>
2019-01-25 14:37:50 +0800

    log

--- android-lua/src/main/java/cm/lua/moon/Logw.java
+++ android-lua/src/main/java/cm/lua/moon/Logw.java
@@ -10,53 +10,53 @@ public class Logw {
     public Logw() {
     }
 
-    public static int d(String str, String str2) {
-        return (isLoggable(str, 3) && LuaJavaAPI.isDebug) ? Log.d(str, str2) : -1;
+    public static int d(String tag, String msg) {
+        return (isLoggable(tag, Log.DEBUG) && LuaJavaAPI.isDebug) ? Log.d(tag, msg) : -1;
     }
 
-    public static int d(String str, String str2, Throwable th) {
-        return (isLoggable(str, 3) && LuaJavaAPI.isDebug) ? Log.d(str, str2, th) : -1;
+    public static int d(String tag, String msg, Throwable tr) {
+        return (isLoggable(tag, Log.DEBUG) && LuaJavaAPI.isDebug) ? Log.d(tag, msg, tr) : -1;
     }
 
-    public static int e(String str, String str2) {
-        return (isLoggable(str, 6) && LuaJavaAPI.isDebug) ? Log.e(str, str2) : -1;
+    public static int e(String tag, String msg) {
+        return (isLoggable(tag, Log.ERROR) && LuaJavaAPI.isDebug) ? Log.e(tag, msg) : -1;
     }
 
-    public static int e(String str, String str2, Throwable th) {
-        return (isLoggable(str, 6) && LuaJavaAPI.isDebug) ? Log.e(str, str2, th) : -1;
+    public static int e(String tag, String msg, Throwable tr) {
+        return (isLoggable(tag, Log.ERROR) && LuaJavaAPI.isDebug) ? Log.e(tag, msg, tr) : -1;
     }
 
-    public static int i(String str, String str2) {
-        return (isLoggable(str, 4) && LuaJavaAPI.isDebug) ? Log.i(str, str2) : -1;
+    public static int i(String tag, String msg) {
+        return (isLoggable(tag, Log.INFO) && LuaJavaAPI.isDebug) ? Log.i(tag, msg) : -1;
     }
 
-    public static int i(String str, String str2, Throwable th) {
-        return (isLoggable(str, 4) && LuaJavaAPI.isDebug) ? Log.i(str, str2, th) : -1;
+    public static int i(String tag, String msg, Throwable tr) {
+        return (isLoggable(tag, Log.INFO) && LuaJavaAPI.isDebug) ? Log.i(tag, msg, tr) : -1;
     }
 
-    private static boolean isLoggable(String str, int i) {
-        return Log.isLoggable(str, i);
+    private static boolean isLoggable(String tag, int level) {
+        return Log.isLoggable(tag, level);
     }
 
-    public static void trace(String str, String str2) {
+    public static void trace(String tag, String msg) {
         if (LuaJavaAPI.isDebug) {
-            System.out.println(str + "::" + str2);
+            System.out.println(tag + "::" + msg);
         }
     }
 
-    public static int v(String str, String str2) {
-        return (isLoggable(str, 2) && LuaJavaAPI.isDebug) ? Log.v(str, str2) : -1;
+    public static int v(String tag, String msg) {
+        return (isLoggable(tag, Log.VERBOSE) && LuaJavaAPI.isDebug) ? Log.v(tag, msg) : -1;
     }
 
-    public static int v(String str, String str2, Throwable th) {
-        return (isLoggable(str, 2) && LuaJavaAPI.isDebug) ? Log.v(str, str2, th) : -1;
+    public static int v(String tag, String msg, Throwable tr) {
+        return (isLoggable(tag, Log.VERBOSE) && LuaJavaAPI.isDebug) ? Log.v(tag, msg, tr) : -1;
     }
 
-    public static int w(String str, String str2) {
-        return (isLoggable(str, 5) && LuaJavaAPI.isDebug) ? Log.w(str, str2) : -1;
+    public static int w(String tag, String msg) {
+        return (isLoggable(tag, Log.WARN) && LuaJavaAPI.isDebug) ? Log.w(tag, msg) : -1;
     }
 
-    public static int w(String str, String str2, Throwable th) {
-        return (isLoggable(str, 5) && LuaJavaAPI.isDebug) ? Log.w(str, str2, th) : -1;
+    public static int w(String tag, String msg, Throwable tr) {
+        return (isLoggable(tag, Log.WARN) && LuaJavaAPI.isDebug) ? Log.w(tag, msg, tr) : -1;
     }
 }
